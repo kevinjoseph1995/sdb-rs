ef459c88592c0d8a75695ef1df00caece3474270
Kevin Joseph <kevinjoseph1995@gmail.com>
2025-06-28 21:56:13 -0700

    Remove unused test binary

--- tools/test_program/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut counter = 0;
-    loop {
-        println!("Hello! {}", counter);
-        counter += 1;
-        std::thread::sleep(std::time::Duration::from_secs(1));
-    }
-}
